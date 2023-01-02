dcf1cadbe697c12d8973d1a252fa99442955c37b
T145 <T145@protonmail.com>
2023-01-01 20:52:45 -0500

    feat(mod): Added content as of v1.8.5

---
+++ mod/descriptor.mod
@@ -0,0 +1,12 @@
+version="1.8.5"
+tags={
+	"Balance"
+	"Events"
+	"Fixes"
+	"Galaxy Generation"
+	"Gameplay"
+	"Graphics"
+}
+name="Actual Stellar Systems"
+supported_version="3.6.*"
+remote_file_id="2904128489"
--- publish.bat
+++ publish.bat
@@ -3,6 +3,6 @@
 FOR /F "tokens=1" %%F IN ('"powershell -co [Environment]::GetFolderPath('MyDocuments')"') DO SET DOCS=%%F
 set DEST=%DOCS%\Paradox Interactive\Stellaris\mod
 
-xcopy "%CD%\mod" "%DEST%\[MODNAME]" /c /g /d /i /e /r /h /y
+xcopy "%CD%\mod" "%DEST%\ActualStellarSystems" /c /g /d /i /e /r /h /y
 
 endlocal && goto :EOF
